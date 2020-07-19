95deb709b302b0746475c2f5511f1965cb52dc05
orpheeh <norpheehounkponou@gmail.com>
2020-07-19 11:35:16 +0100

    feat

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function toggleNavMenu(){
+function toggleNavMenu() {
     document.querySelector('.nav-bar').classList.toggle('show-nav');
 }
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3JwaGVlIiwiYSI6ImNrY3N3a3IxdDFzdmUyeWx1aDg2amZyZncifQ.PzEdiWnGFTsJ4dLKADBw5Q';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [9.4279255, 0.411846], // starting position
+    zoom: 17
+});
+
+var marker = new mapboxgl.Marker()
+.setLngLat([9.4279255, 0.411846])
+.addTo(map);
+
+map.addControl(new mapboxgl.NavigationControl());
